82539756143c1cbca8c41070aafc1960b7f00093
knucklesx2 <31361048+knucklesx2@users.noreply.github.com>
2017-12-20 17:06:03 -0500

    clean up images

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ $(document).ready(function () {
         $(this).hide();
     });
 });
+
+
+
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
